7ba12ea4758a6045457b4cb871aa0731bcce0ac2
Matteo Ferrarese <matteo.ferrarese@yahoo.it>
2021-02-02 22:36:24 +0100

    Migrations + factory + model + seed

---
+++ drink/database/migrations/2021_02_02_212021_create_drinks_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDrinksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('drinks', function (Blueprint $table) {
+            $table -> id();
+
+            $table -> string('name', 50);
+            $table -> integer('alcohol_content') -> unsigned();
+            $table -> float('price') -> unsigned();
+
+            $table -> timestamps(); 
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('drinks');
+    }
+}
